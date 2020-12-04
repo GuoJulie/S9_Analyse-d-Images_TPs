5558e7efc47657528c717457ab22fea566d76534
yuanyuan <yuanyuan.liu.c03@gmail.com>
2020-12-04 17:18:12 +0100

    import error

--- TP_2020/partie1/image_statistique.py
+++ TP_2020/partie1/image_statistique.py
@@ -9,7 +9,6 @@ def img_hist(img):
     plt.imshow(img, 'gray')
     plt.xticks([])
     plt.yticks([])
-    plt.title("Original")
     plt.subplot(122)
     plt.hist(img.ravel(), 256, [0, 256])
     plt.show()
@@ -24,16 +23,32 @@ def img_equ(img):
     cv2.waitKey()
     cv2.destroyAllWindows()
 
+def img_gris(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    return img
+
 if __name__ == '__main__':
     # Obtenir le path du fichier python
     rep_cour = os.path.dirname(os.getcwd())
     print(rep_cour)
 
-    path = rep_cour + '/data/lisa.png'
+    path_lisa = rep_cour + '/data/lisa.png'
+    path_vache = rep_cour + '/data/vache.jpg'
+    path_paysage = rep_cour + '/data/paysage.jpg'
+    path_boat = rep_cour + '/data/boats.jpg'
+    path_soleil = rep_cour + '/data/soleil.png'
     # Lire une image et changer le couleur en gris au travers de cvtColor
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img_hist(img)
-    img_equ(img)
-    img_hist(equ)
+    img_lisa = img_gris(path_lisa)
+    img_vache = img_gris(path_vache)
+    img_paysage = img_gris(path_paysage)
+    img_boat = img_gris(path_boat)
+    img_soleil = img_gris(path_soleil)
+
+    img_hist(img_lisa)
+    img_hist(img_boat)
+    img_equ(img_boat)
+    img_hist(img_boat)
+
